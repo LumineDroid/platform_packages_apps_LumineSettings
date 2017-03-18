8c963ab6f189aca295c70eb702b61ccd1f21c672
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    LumineSettings: Add ability to toggle bluetooth battery level [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id725cf7ed6c26131baf36e826928d907ef6f46d6
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/luminedroid/extensions/category/statusbar/StatusBarSettings.java
+++ src/org/luminedroid/extensions/category/statusbar/StatusBarSettings.java
@@ -22,14 +22,32 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.luminedroid.preferences.SystemSettingSwitchPreference;
+import org.luminedroid.utils.DeviceUtils;
+
 @SearchIndexable
 public class StatusBarSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
+
+    private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
+    private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
+
+    private PreferenceCategory mIconsCategory;
+    private SystemSettingSwitchPreference mBluetoothBatteryStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.extensions_statusbar);
+
+        final Context context = getContext();
+
+        mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
+        mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
+
+        if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+            mIconsCategory.removePreference(mBluetoothBatteryStatus);
+        }
     }
 
     @Override
@@ -47,6 +65,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
+                    if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+                        keys.add(KEY_BLUETOOTH_BATTERY_STATUS);
+                    }
                     return keys;
                 }
             };

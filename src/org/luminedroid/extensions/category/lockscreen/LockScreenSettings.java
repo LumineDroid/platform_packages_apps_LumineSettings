c26632682cd1c71fc708b608a053e24f4ed32d1f
Stealth1226 <rishawn@projectlighthouse.tech>
2024-04-26 13:15:05 +0900

    LumineSettings: Allow disabling ripple effect on unlock [2/2]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I50d18ad138e75b1e2b5bf20f87edaba2d16df6dc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/luminedroid/extensions/category/lockscreen/LockScreenSettings.java
+++ src/org/luminedroid/extensions/category/lockscreen/LockScreenSettings.java
@@ -26,6 +26,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment
   private static final String KEY_FINGERPRINT_CATEGORY = "lock_screen_fingerprint_category";
   private static final String KEY_AUTHENTICATION_SUCCESS = "fp_success_vibrate";
   private static final String KEY_AUTHENTICATION_ERROR = "fp_error_vibrate";
+  private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
 
   private PreferenceCategory mFingerprintCategory;
 
@@ -69,6 +70,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment
           if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
             keys.add(KEY_AUTHENTICATION_SUCCESS);
             keys.add(KEY_AUTHENTICATION_ERROR);
+            keys.add(KEY_RIPPLE_EFFECT);
           }
           return keys;
         }

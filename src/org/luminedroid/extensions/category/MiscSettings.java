d01452a69476a2cc48e81db5f357d7c0d2b14fff
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-02-03 14:20:07 +0000

    LumineSettings: Automate `pif.json` download and import feature
    - Get rid of manual download and loading the json file
    - Cleanup some files
    - Cleanup some codes
    
    Change-Id: I9dadfd44cbca93f7bd14d699bcb4cc5c4053aee7

--- src/org/luminedroid/extensions/category/MiscSettings.java
+++ src/org/luminedroid/extensions/category/MiscSettings.java
@@ -6,13 +6,9 @@
 package org.luminedroid.extensions.category;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -46,10 +42,7 @@ public class MiscSettings extends SettingsPreferenceFragment
     private static final String KEY_PIF_JSON_MANAGE_PREFERENCE = "pif_json_manage_preference";
     private Preference mPifJsonManagePreference;
     private Handler mHandler;
-
     private Context context;
-    private Resources resources;
-    private ContentResolver resolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,108 +50,76 @@ public class MiscSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.extensions_misc);
 
         context = getContext();
-        resources = context.getResources();
-        resolver = context.getContentResolver();
-
         mHandler = new Handler();
         mPifJsonManagePreference = findPreference(KEY_PIF_JSON_MANAGE_PREFERENCE);
 
         if (mPifJsonManagePreference != null) {
-            mPifJsonManagePreference.setOnPreferenceChangeListener(this);
+            mPifJsonManagePreference.setOnPreferenceClickListener(preference -> {
+                downloadAndLoadPifJson(context);
+                return true;
+            });
         }
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mPifJsonManagePreference) {
-            onPifManageScreen(context);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    private void onPifManageScreen(Context context) {
-        final String[] items = {
-            resources.getString(R.string.spoofing_pif_json_download_title),
-            resources.getString(R.string.spoofing_pif_json_select_title),
-        };
-        new AlertDialog.Builder(context)
-            .setTitle(resources.getString(R.string.spoofing_pif_manage_title))
-            .setItems(items, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (which == 0) {
-                        downloadPifJson(context);
-                    } else if (which == 1) {
-                        selectPifJson(context);
-                    }
-                }
-            })
-            .show();
-    }
-    private void downloadPifJson(Context context) {
+    private void downloadAndLoadPifJson(Context context) {
         DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(resources.getString(R.string.spoofing_pif_json_download_url));
-        String mes = resources.getString(R.string.spoofing_pif_json_download_message);
+        Uri uri = Uri.parse(context.getResources().getString(R.string.spoofing_pif_json_download_url));
+        String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/pif.json";
+        
         DownloadManager.Request request = new DownloadManager.Request(uri)
             .setTitle("pif.json")
-            .setDescription(mes)
+            .setDescription(context.getResources().getString(R.string.spoofing_pif_json_download_message))
             .setMimeType("application/json")
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "pif.json");
-        downloadManager.enqueue(request);
-        Toast.makeText(context, resources.getString(R.string.spoofing_pif_json_download_message), Toast.LENGTH_LONG).show();
-    }
-    private void selectPifJson(Context context) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("application/json");
-        startActivityForResult(intent, 10001);
+            .setDestinationUri(Uri.parse("file://" + filePath));
+        
+        long downloadId = downloadManager.enqueue(request);
+        
+        new Thread(() -> {
+            boolean downloading = true;
+            while (downloading) {
+                DownloadManager.Query query = new DownloadManager.Query();
+                query.setFilterById(downloadId);
+                try (android.database.Cursor cursor = downloadManager.query(query)) {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                        if (cursor.getInt(columnIndex) == DownloadManager.STATUS_SUCCESSFUL) {
+                            downloading = false;
+                            mHandler.post(() -> loadPifJson(Uri.parse("file://" + filePath)));
+                        }
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
-            Uri uri = data.getData();
-            Log.d(TAG, "URI received: " + uri.toString());
-            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
-                if (inputStream != null) {
-                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                    Log.d(TAG, "JSON data: " + json);
-                    JSONObject jsonObject = new JSONObject(json);
-                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                        String key = it.next();
-                        String value = jsonObject.getString(key);
-                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
-                        SystemProperties.set("persist.sys.pihooks_" + key, value);
-                    }
-                    Toast.makeText(
-                        getContext(),
-                        getContext().getResources().getString(R.string.spoofing_pif_json_select_success),
-                        Toast.LENGTH_LONG
-                    ).show();
+    private void loadPifJson(Uri uri) {
+        try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
+            if (inputStream != null) {
+                String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+                Log.d(TAG, "JSON data: " + json);
+                JSONObject jsonObject = new JSONObject(json);
+                for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    String value = jsonObject.getString(key);
+                    Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
+                    SystemProperties.set("persist.sys.pihooks_" + key, value);
                 }
-            } catch (Exception e) {
-                Log.e(TAG, "Error reading JSON or setting properties", e);
-                Toast.makeText(
-                        getContext(),
-                        getContext().getResources().getString(R.string.spoofing_pif_json_select_failure),
-                        Toast.LENGTH_LONG
-                    ).show();
+                Toast.makeText(getContext(), R.string.spoofing_pif_json_select_success, Toast.LENGTH_LONG).show();
             }
+        } catch (Exception e) {
+            Log.e(TAG, "Error reading JSON or setting properties", e);
+            Toast.makeText(getContext(), R.string.spoofing_pif_json_select_failure, Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // final Context context = getContext();
-        context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
         return false;
     }
 

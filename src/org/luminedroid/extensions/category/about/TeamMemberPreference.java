da0778822aa508ad1a708ff5d528e3726e87ec41
HexaBoo <hexaboo@outlook.com>
2025-06-09 08:02:16 +0000

    LumineSettings: Improve about LumineDroid
    Change-Id: I97feb169ba1062a9f90871702ca29b6687571278

--- src/org/luminedroid/extensions/category/about/TeamMemberPreference.java
+++ src/org/luminedroid/extensions/category/about/TeamMemberPreference.java
@@ -6,15 +6,22 @@
 package org.luminedroid.extensions.category.about;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
 import com.android.settings.R;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 public class TeamMemberPreference extends Preference {
 
@@ -33,12 +40,30 @@ public class TeamMemberPreference extends Preference {
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
+
         ImageView imageView = (ImageView) holder.findViewById(R.id.team_avatar);
         if (imageView != null && imageUrl != null) {
             Glide.with(getContext())
-                 .load(imageUrl)
-                 .apply(RequestOptions.circleCropTransform())
-                 .into(imageView);
+                .load(imageUrl)
+                .apply(RequestOptions.circleCropTransform())
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                Target<Drawable> target, boolean isFirstResource) {
+                        Log.e("Glide", "Image load failed for URL: " + imageUrl, e);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model,
+                                                   Target<Drawable> target,
+                                                   com.bumptech.glide.load.DataSource dataSource,
+                                                   boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .into(imageView);
         }
     }
 }

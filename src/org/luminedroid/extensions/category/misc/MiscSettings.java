d01f16dff7b42d81aa64c842ab58fae4102b87f4
Abhay Singh Gill <abhaygill017@gmail.com>
2025-06-11 15:30:40 +0530

    LumineSettings: Add key generation for keybox spoofing [2/2]
    Change-Id: Ida88f8612ae8aec53316b02ee92a81defc80bd9a
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- src/org/luminedroid/extensions/category/misc/MiscSettings.java
+++ src/org/luminedroid/extensions/category/misc/MiscSettings.java
@@ -5,9 +5,15 @@
 
 package org.luminedroid.extensions.category.misc;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
@@ -26,10 +32,32 @@ import java.util.List;
 public class MiscSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String KEYBOX_DATA_KEY = "keybox_data_setting";
+    private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
+    private KeyboxDataPreference mKeyboxDataPreference;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.extensions_misc);
+
+        mKeyboxFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    Preference pref = findPreference(KEYBOX_DATA_KEY);
+                    if (pref instanceof KeyboxDataPreference) {
+                        ((KeyboxDataPreference) pref).handleFileSelected(uri);
+                    }
+                }
+            }
+        );
+
+        mKeyboxDataPreference = findPreference(KEYBOX_DATA_KEY);
+        if (mKeyboxDataPreference != null) {
+            mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
+        }
     }
 
     @Override

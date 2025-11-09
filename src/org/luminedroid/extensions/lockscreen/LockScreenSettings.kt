50bb340410dea2beb785c0430b5bed5d3630a7dc
Pranav Vashi <neobuddy89@gmail.com>
2025-11-09 09:19:46 +0000

    LumineSettings: Add proper toggle for smartspace [2/2]
    Change-Id: Ic6aaaa5aec244a2793bbe06ef30c807c9584a338
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- src/org/luminedroid/extensions/lockscreen/LockScreenSettings.kt
+++ src/org/luminedroid/extensions/lockscreen/LockScreenSettings.kt
@@ -27,12 +27,16 @@ class LockScreenSettings :
     SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener, Indexable {
 
     private var weather: SwitchPreferenceCompat? = null
+    private var smartspace: SwitchPreferenceCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.extensions_lockscreen)
 
         val prefScreen: PreferenceScreen = preferenceScreen
+
+        smartspace = findPreference(KEY_SMARTSPACE)
+        smartspace?.onPreferenceChangeListener = this
 
         weather = findPreference(KEY_WEATHER)
         weather?.onPreferenceChangeListener = this
@@ -42,6 +46,13 @@ class LockScreenSettings :
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
         when (preference) {
+            smartspace -> {
+                smartspace?.isChecked = newValue as Boolean
+                updateWeatherSettings()
+                SystemUtils.showSystemUiRestartDialog(requireContext())
+                return true
+            }
+
             weather -> {
                 weather?.isChecked = newValue as Boolean
                 SystemUtils.showSystemUiRestartDialog(requireContext())
@@ -62,12 +73,13 @@ class LockScreenSettings :
 
     private fun updateWeatherSettings() {
         val weatherPref = weather ?: return
+        val smartspacePref = smartspace ?: return
 
         val weatherEnabled = OmniJawsClient.get().isOmniJawsEnabled(requireContext())
 
         weatherPref.isEnabled = !smartspacePref.isChecked && weatherEnabled
         weatherPref.setSummary(
-            if (weatherEnabled) R.string.lockscreen_weather_summary
+            if (!smartspacePref.isChecked && weatherEnabled) R.string.lockscreen_weather_summary
             else R.string.lockscreen_weather_enabled_info
         )
     }
@@ -75,9 +87,17 @@ class LockScreenSettings :
     companion object {
 
         private const val KEY_WEATHER = "lockscreen_weather_enabled"
+        private const val KEY_SMARTSPACE = "lockscreen_smartspace_enabled"
 
         fun reset(context: Context) {
             val resolver: ContentResolver = context.contentResolver
+
+            Settings.Secure.putIntForUser(
+                resolver,
+                Settings.Secure.LOCKSCREEN_SMARTSPACE_ENABLED,
+                1,
+                UserHandle.USER_CURRENT,
+            )
 
             Settings.System.putIntForUser(
                 resolver,

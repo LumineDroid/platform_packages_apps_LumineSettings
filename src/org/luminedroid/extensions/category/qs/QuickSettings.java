27c527ce915634ea351be5c7dd11badbd6b71aaa
Abhay Singh Gill <abhaygill017@gmail.com>
2025-11-10 17:05:39 +0000

    LumineSettings: Allow to change brightness slider positioning in new compose QS [2/2]
    neobuddy89:
    * Adapt for lineage settings
    * Use onStop() instead onDestroy()
    * Register tunable for current user
    * Polish a bit
    
    Change-Id: I8345c47ed52188174a3aee39768738fc17466800
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/luminedroid/extensions/category/qs/QuickSettings.java
+++ src/org/luminedroid/extensions/category/qs/QuickSettings.java
@@ -5,9 +5,14 @@
 
 package org.luminedroid.extensions.category.qs;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -15,20 +20,62 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
+import lineageos.providers.LineageSettings;
 
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment
     implements Preference.OnPreferenceChangeListener, Indexable {
 
+  private static final String QS_BRIGHTNESS_CATEGORY = "qs_brightness_slider_category";
+  private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+  private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
+
+  private ListPreference mShowBrightnessSlider;
+  private ListPreference mBrightnessSliderPosition;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     addPreferencesFromResource(R.xml.extensions_quicksettings);
+
+    final Context context = getContext();
+    final ContentResolver resolver = context.getContentResolver();
+
+    PreferenceCategory brightnessCategory =
+        (PreferenceCategory) findPreference(QS_BRIGHTNESS_CATEGORY);
+
+    mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
+    mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+    boolean showSlider =
+        LineageSettings.Secure.getIntForUser(
+                resolver,
+                LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER,
+                1,
+                UserHandle.USER_CURRENT)
+            > 0;
+
+    mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
+    mBrightnessSliderPosition.setEnabled(showSlider);
   }
 
   @Override
   public boolean onPreferenceChange(Preference preference, Object newValue) {
+    ContentResolver resolver = getContext().getContentResolver();
+
+    if (preference == mShowBrightnessSlider) {
+      int value = Integer.parseInt((String) newValue);
+      mBrightnessSliderPosition.setEnabled(value > 0);
+      return true;
+    }
     return false;
+  }
+
+  public static void reset(Context mContext) {
+    ContentResolver resolver = mContext.getContentResolver();
+    LineageSettings.Secure.putIntForUser(
+        resolver, LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT);
+    LineageSettings.Secure.putIntForUser(
+        resolver, LineageSettings.Secure.QS_BRIGHTNESS_SLIDER_POSITION, 0, UserHandle.USER_CURRENT);
   }
 
   @Override

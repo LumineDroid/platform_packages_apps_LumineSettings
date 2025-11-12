c674e43f232ddd1909a8dbbe482a183d7f96de04
Pranav Vashi <neobuddy89@gmail.com>
2025-11-12 09:47:40 +0000

    LumineSettings: Implement spoofing features
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Change-Id: Ie43e708c19f71cd1f9a4e7cb22754600ffcd5249
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- src/org/luminedroid/utils/DeviceUtils.java
+++ src/org/luminedroid/utils/DeviceUtils.java
@@ -311,4 +311,10 @@ public class DeviceUtils {
     }
     return true;
   }
+
+  public static boolean isCurrentlySupportedPixel() {
+    boolean isPixelDevice =
+        SystemProperties.get("ro.product.model").matches("Pixel (3|4|5|6|7|8|9|10)[a-zA-Z ]*");
+    return isPixelDevice;
+  }
 }

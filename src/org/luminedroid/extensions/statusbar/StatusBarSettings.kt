f28191d2512fed5a95b3b2466376ca46217bd34f
Hexaboo <hexaboo@outlook.com>
2026-01-17 20:02:57 +0000

    LumineSettings: Show restart systemui dialog for colored icons
    - Adapted fron https://github.com/Evolution-X/packages_apps_Evolver/commit/7fb0afd038d0fde8954cc1cce139064231faa073
    
    Change-Id: I5ebc1c05e01956760d96941677f97be9bab3b1e7
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- src/org/luminedroid/extensions/statusbar/StatusBarSettings.kt
+++ src/org/luminedroid/extensions/statusbar/StatusBarSettings.kt
@@ -14,17 +14,32 @@ import com.android.settings.SettingsPreferenceFragment
 import com.android.settings.search.BaseSearchIndexProvider
 import com.android.settingslib.search.Indexable
 import com.android.settingslib.search.SearchIndexable
+import org.luminedroid.preferences.SystemSettingSwitchPreference
+import org.luminedroid.utils.SystemUtils
 
 @SearchIndexable
 class StatusBarSettings : SettingsPreferenceFragment(),
     Preference.OnPreferenceChangeListener, Indexable {
 
+    private lateinit var coloredIcons: SystemSettingSwitchPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.extensions_statusbar)
+
+        coloredIcons = findPreference(KEY_COLORED_ICONS)!!
+        coloredIcons.onPreferenceChangeListener = this
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        val context = requireContext()
+
+        when (preference) {
+            coloredIcons -> {
+                SystemUtils.showSystemUiRestartDialog(context)
+                return true
+            }
+        }
         return false
     }
 
@@ -33,6 +48,8 @@ class StatusBarSettings : SettingsPreferenceFragment(),
     }
 
     companion object {
+        private const val KEY_COLORED_ICONS = "statusbar_colored_icons"
+
         @JvmField
         val SEARCH_INDEX_DATA_PROVIDER = object : BaseSearchIndexProvider(R.xml.extensions_statusbar) {
             override fun getNonIndexableKeys(context: Context): List<String> {

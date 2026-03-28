5347c6360c880b5b5f84fc005259055bb7ad1713
Dmitrii <bankersenator@gmail.com>
2026-03-28 15:23:25 +0000

    LumineSettings: fix crash in hideapplist
    Change-Id: I8ab08e368745aaea9623bd048587e6e97a24570e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/org/luminedroid/extensions/misc/HideAppListStatusSettings.kt
+++ src/org/luminedroid/extensions/misc/HideAppListStatusSettings.kt
@@ -211,12 +211,14 @@ class HideAppListSettings : Fragment(R.layout.hide_applist_layout) {
     private fun getKey(): String = Settings.Secure.HIDE_APPLIST
 
     private fun refreshList() {
+        if (!isAdded || context == null) return
+
         var list =
             packageList
                 .filter {
                     if (!showSystem) {
                         !it.applicationInfo!!.isSystemApp() &&
-                            !resources
+                            !requireContext().resources
                                 .getStringArray(R.array.hide_applist_hidden_apps)
                                 .asList()
                                 .contains(it.applicationInfo!!.packageName) &&

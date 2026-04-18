1a0f0ac8e2226b75d1337b54e9a467e6e4933b3b
Hexaboo <hexaboo@outlook.com>
2026-04-18 08:14:39 +0000

    Revert "LumineSettings: Add ongoing action chip implementation"
    This reverts commit 3b9599cb8044dcc3b0a3620858eeea8b1d45a584.

--- src/org/luminedroid/extensions/statusbar/StatusBarSettings.kt
+++ src/org/luminedroid/extensions/statusbar/StatusBarSettings.kt
@@ -5,7 +5,6 @@
 
 package org.luminedroid.extensions.statusbar
 
-import android.content.ContentResolver
 import android.content.Context
 import android.os.Bundle
 import androidx.preference.Preference
@@ -47,12 +46,6 @@ class StatusBarSettings :
 
     companion object {
         private const val KEY_COLORED_ICONS = "statusbar_colored_icons"
-
-        fun reset(context: Context) {
-            val resolver: ContentResolver = context.contentResolver
-
-            OngoingProgressBar.reset(context)
-        }
 
         @JvmField
         val SEARCH_INDEX_DATA_PROVIDER =
